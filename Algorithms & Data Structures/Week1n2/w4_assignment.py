53ba2cb760304d1e2c3adfe73a75e01cf56c8fe5
ireneannx <irene2104@gmail.com>
2021-09-17 10:08:58 +0200

    week4 OOP assignment outline

---
+++ Algorithms & Data Structures/Week1n2/w4_assignment.py
@@ -0,0 +1,28 @@
+# google style guide for python PEP8 and PEP20
+# Object oriented programing
+
+class Node:
+
+    def __init__(self, date):
+        self.data = data
+        self.next = None
+
+    def __str__(self):
+        '...'
+
+
+class LinkedList:
+    def __init__(self, ...):
+        ...
+    def __str__(self): #to loop thru n show some kinda separator so u can see output easy
+        ''
+    def add(self, data): #starting at the head and youre going from one elem to the next and then at the last element u add the data. add is insert at the end
+        ''''''
+    def at(self, index): #if youre at the fiest half start at the head, 2nd half start at tail
+        ''''''
+    def insert(self, index, data): #insert a node
+        ''''''
+    def remove(self, index): #remove a node. make sure the elements before n after deleted node are still connected
+        ''''''
+    def __len__(self): #len of list
+        ''''''
