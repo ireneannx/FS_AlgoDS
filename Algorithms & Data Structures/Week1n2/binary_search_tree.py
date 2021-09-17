74c88d09882e2e27781fbf87defbf1858a1410e9
ireneannx <irene2104@gmail.com>
2021-09-17 12:30:03 +0200

    week4 binary search try (unbalanced) - added display func from vahe

--- Algorithms & Data Structures/Week1n2/binary_search_tree.py
+++ Algorithms & Data Structures/Week1n2/binary_search_tree.py
@@ -1,5 +1,7 @@
 # making a binary search tree class
 
+# -------------------------------------------------------------------------------------------
+# UNBALANCED BINARY SEARCH TREE
 class Node:
     def __init__(self, key, data):
         self.key = key
@@ -28,7 +30,7 @@ class Node:
             if self.left is None:
                 self.left = Node(key, data)
             else:
-                self.left.insert(key,data)
+                self.left.insert(key, data)
 
     def get(self, key):
         if self.key == key:
@@ -51,6 +53,57 @@ class Node:
             else:
                 return self.left.get(key)
 
+    # function to display binary search tree -from Vahe ---------------------
+    def display(self):
+        lines, *_ = self._display_aux()
+        for line in lines:
+            print(line)
+
+    def _display_aux(self):
+        """Returns list of strings, width, height, and horizontal coordinate of the root."""
+        # No child.
+        if self.right is None and self.left is None:
+            line = '%s' % self.key
+            width = len(line)
+            height = 1
+            middle = width // 2
+            return [line], width, height, middle
+
+        # Only left child.
+        if self.right is None:
+            lines, n, p, x = self.left._display_aux()
+            s = '%s' % self.key
+            u = len(s)
+            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
+            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+            shifted_lines = [line + u * ' ' for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+        # Only right child.
+        if self.left is None:
+            lines, n, p, x = self.right._display_aux()
+            s = '%s' % self.key
+            u = len(s)
+            first_line = s + x * '_' + (n - x) * ' '
+            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+            shifted_lines = [u * ' ' + line for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+        # Two children.
+        left, n, p, x = self.left._display_aux()
+        right, m, q, y = self.right._display_aux()
+        s = '%s' % self.key
+        u = len(s)
+        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
+        second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+        if p < q:
+            left += [n * ' '] * (q - p)
+        elif q < p:
+            right += [m * ' '] * (p - q)
+        zipped_lines = zip(left, right)
+        lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
+        return lines, n + m + u, max(p, q) + 2, n + u // 2
+
 
 root = Node("M", 24)
 root.insert("R", 22)
@@ -62,3 +115,7 @@ root.get("R")
 root.get("S")
 root.get("V")
 root.get("P")
+
+root.display()
+
+# ------------------------------------ UNBALANCED BINARY SEARCH TREE END
