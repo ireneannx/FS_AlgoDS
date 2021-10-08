537f0cf47829c6d932fb2c79927b66e22e6403cd
ireneannx <irene2104@gmail.com>
2021-10-09 01:28:00 +0200

    implemented choose function

--- Algorithms & Data Structures/Week4-5/211002_stable_matching.py
+++ Algorithms & Data Structures/Week4-5/211002_stable_matching.py
@@ -8,9 +8,12 @@ class Person:
         # self.status = 0 #engaged(1) or single(0)
         self.partner = None
 
-    def __str__(self):
-        return str(self.name)  # for printing node(s) easily
+    def __str__(self): 
+        # for petinting nodes easily
+        # s = self.name self.partner}'
+        return str(self.name)
 
+    
     def set_preference(self, list_of_persons):  # make into dict later
         self.preference = list_of_persons
 
@@ -53,12 +56,11 @@ B.set_preference(prefB)
 C.set_preference(prefA)
 
 
-# print(A.preference[1])
 
 
-def return_index(list_, guy):
+def return_index(list_, person):
     for i in range(len(list_)):
-        if list_[i] == guy:
+        if list_[i] == person:
             return i
     Exception('index out of range!!!!!!')
 
@@ -74,24 +76,45 @@ def choose(girl, potential_guy):
         current_guy_index = return_index(preferences, current_guy)
         potential_guy_index = return_index(preferences, potential_guy)
         print(current_guy_index, potential_guy_index)
+        girl_index = return_index(current_guy.preference, girl)
 
-        if current_guy_index > potential_guy_index:
+        if potential_guy_index > current_guy_index:
             print(f'sorry, {potential_guy} has been rejected by {girl}')
         #     remove girl from guys list
+            potential_guy.preference[girl_index] = None
         else:
-            print()
+            print(f'congratulations! {potential_guy} has been accepted by {girl}')
+            # remove girl from prev guys list 
+            print(f'ex index = {girl_index}')
+            current_guy.preference[girl_index] = None
+            current_guy.partner = None 
+
+            # set girl up with new guy 
+            potential_guy.partner = girl
+            girl.partner = potential_guy
 
 
-        return 0
-
-
-#
-def propose(guy):
-    preferences = guy.preference
-    preferred_girl = preferences[0]
-    choose(preferred_girl, guy)
     return 0
 
 
-choose(A, b)
-choose(A, c)
+#
+# def make_proposal(boy):
+#     preferences = person.preference
+#     next_choice = preferences[0]
+#     choose(next_choice, boy)
+#     return 0
+
+
+
+print(A)
+print(a)
+choose(A, a)
+print(f'{A} partner-{A.partner} pref-{A.preference}')
+# print(f'{a} partner-{a.partner} pref-{a.preference}')
+choose(B,b)
+choose(A,c)
+print(f'{A} partner-{A.partner} pref-{A.preference}')
+print(C.partner)
+print(f'{a} partner-{a.partner} pref-{a.preference}')
+
+
